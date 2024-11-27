b3a116e6ab70f38b9230306fdb5beb520e1fd6f2
Yusuf Talha Kılıç <talha.kilic@priviasecurity.com>
2024-11-27 19:40:03 +0300

    feat(validation): add reusable struct validation function using validator.v10

--- src/api/validator.go
+++ src/api/validator.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+)
+
+var validate = validator.New()
+
+func ValidateStruct(s interface{}) []string {
+	err := validate.Struct(s)
+	if err != nil {
+		var errorMessages []string
+		for _, err := range err.(validator.ValidationErrors) {
+			msg := fmt.Sprintf("Field '%s' failed validation: %s\n", err.Field(), err.Tag())
+			errorMessages = append(errorMessages, msg)
+		}
+		return errorMessages
+	}
+	return nil
+}
