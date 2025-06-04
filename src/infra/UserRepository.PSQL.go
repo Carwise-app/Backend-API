1267b737f9abe63565595de2bcb954d39f494ae2
Yusuf Talha Kılıç <talha.kilic@priviasecurity.com>
2025-06-04 16:15:34 +0300

    Fix SQL query in Create method of UserRepository: remove redundant parameter for improved clarity and maintainability in user creation process.

--- src/infra/UserRepository.PSQL.go
+++ src/infra/UserRepository.PSQL.go
@@ -140,7 +140,7 @@ func (r *UserRepository) Create(user *carwise.User) error {
 			email_notify,
 			push_notify
 		) VALUES (
-			$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18
+			$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17
 		)`
 	_, err := r.db.Exec(query,
 		user.Id,
